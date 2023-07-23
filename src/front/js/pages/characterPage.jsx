981b6b9329ace82770c443748da6defa6fc7fc99
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-07-23 00:12:19 +0000

    Fetch API for characterPage

--- src/front/js/pages/characterPage.jsx
+++ src/front/js/pages/characterPage.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
 const CharacterPage = () => {
     const {charID} = useParams()
+    const [character, setCharacter] = useState({})
+
+    useEffect(() => {getPerson()}, [])
+
+    const url = `https://swapi.dev/api/people/${charID}/`
+
+    const getPerson = () => {
+        fetch(url, {method: 'GET'})
+        .then(response => response.json())
+        .then(person => setCharacter(person))
+    }
 
     const imgStyle = {
         width: "90%",
         aspectRatio: "3/2",
     }
+    
+    const cmToFt = (height) => {
+        let inches = height * 0.39
+        let feet = Math.floor(inches/12)
+        let remainder = Math.floor(inches) - (feet * 12)
+        return `${feet}'${remainder}`
+    }
+
+    console.log(character)
 
     return (
         <div className='row p-5'>
@@ -15,8 +35,15 @@ const CharacterPage = () => {
                 <img className='ms-auto' src="https://placehold.co/600x400" style={imgStyle}></img>
             </div>
             <div className="col-6">
-                <h2>{charID}</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h2>{character.name}</h2>
+                <ul>
+                    <li>Gender: {character.gender}</li>
+                    <li>Birth Year: {character.birth_year}</li>
+                    <li>Height: {cmToFt(character.height)}</li>
+                    <li>Hair Color: {character.hair_color}</li>
+                    <li>Eye Color: {character.eye_color}</li>
+                    <li></li>
+                </ul>
             </div>
         </div>
     )
